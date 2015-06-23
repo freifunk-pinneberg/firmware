b4972dab496d3eedc175e4a6e63c2b0fb5ab10c6
Freifunk Community Pinneberg <pirat@hoogi.de>
2015-06-23 09:51:35 +0200

    Changed release to beta1

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.7+exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 0.7+beta1
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
