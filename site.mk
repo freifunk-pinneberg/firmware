08f6c539838ef2a0c94ae047c1a9fa0e74b3e1b4
Kevin Quiatkowski <kontakt@kevinquiatkowski.de>
2024-09-10 13:22:34 +0200

    Fix domain select

--- site.mk
+++ site.mk
@@ -9,7 +9,7 @@ GLUON_SITE_PACKAGES := \
     gluon-config-mode-geo-location \
     gluon-config-mode-hostname \
     gluon-config-mode-mesh-vpn \
-    gluon-hoodselector \
+    gluon-config-mode-domain-select \
     gluon-scheduled-domain-switch \
     gluon-ebtables-filter-multicast \
     gluon-ebtables-filter-ra-dhcp \
